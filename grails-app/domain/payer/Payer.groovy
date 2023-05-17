6ff3da7349554c0f5e628bd8de00b8664b96809d
HugoSRCruz <hugo.cruz@asaas.com.br>
2023-05-17 11:41:30 -0300

    Prototipo do dominio Payer

--- grails-app/domain/payer/Payer.groovy
+++ grails-app/domain/payer/Payer.groovy
@@ -1,24 +1,16 @@
 package payer
 
-import java.time.LocalDate
-
 class Payer {
 
-    Long id
     String name
     String email
-    String celular
     String cpfCnpj
-    boolean deleted
-    LocalDate createdAt
 
     static mapping = {
-        id generator: 'identity'
         cpfCnpj unique: true, length: 14
-        version(false)
     }
     static constraints = {
         name blank: false
-        email blank: false
+        email blank: false, email: true
     }
 }
