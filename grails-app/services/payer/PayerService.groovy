1f0929eb94d770633af715889f4a187520db8ab5
HugoSRCruz <hugo.cruz@asaas.com.br>
2023-05-18 13:21:49 -0300

    Criando PayerService

---
+++ grails-app/services/payer/PayerService.groovy
@@ -0,0 +1,31 @@
+package payer
+
+import grails.gorm.transactions.Transactional
+
+@Transactional
+class PayerService {
+
+    List<Payer> findAll(){
+        return Payer.list()
+    }
+
+    Payer findById(Long id){
+        return Payer.get(id)
+    }
+
+    void save(params){
+
+        if (!params.name)
+            throw new RuntimeException("Name is mandatory!")
+        if (!params.email)
+            throw new RuntimeException("Name is mandatory!")
+        if (!params.cpfCnpj)
+            throw new RuntimeException("Name is mandatory!")
+
+        Payer payer = new Payer()
+        payer.name = params.name
+        payer.email = params.email
+        payer.cpfCnpj = params.cpfCnpj
+        payer.save()
+    }
+}
