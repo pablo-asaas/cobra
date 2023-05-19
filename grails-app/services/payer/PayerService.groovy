cd34295c265175ec63dcdbdf08593eaf05b4ee82
HugoSRCruz <hugo.cruz@asaas.com.br>
2023-05-19 12:01:29 -0300

    Retorno especifico por tipo de validação

--- grails-app/services/payer/PayerService.groovy
+++ grails-app/services/payer/PayerService.groovy
@@ -17,12 +17,10 @@ class PayerService {
 
     public void save(params){
 
-        if (!params.name)
-            throw new RuntimeException("Name is mandatory!")
-        if (!params.email)
-            throw new RuntimeException("Name is mandatory!")
-        if (!params.cpfCnpj)
-            throw new RuntimeException("Name is mandatory!")
+        if (!params.name) throw new RuntimeException("Nome é obrigatório")
+        if (!params.email) throw new RuntimeException("Email é obrigatório")
+        if (!params.cpfCnpj) throw new RuntimeException("CPF/CNPJ é obrigatório")
+        if (!params.phoneNumber) throw new RuntimeException("Celular é obrigatório")
 
         Payer payer = new Payer()
         payer.name = params.name
