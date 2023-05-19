c4bf3acb9c49780d154206ca8ff721060ac0426f
Pablo <pablo.bayerl@asaas.com.br>
2023-05-19 11:17:15 -0300

    Adicionado método save

--- grails-app/controllers/customer/CustomerController.groovy
+++ grails-app/controllers/customer/CustomerController.groovy
@@ -4,11 +4,23 @@ class CustomerController {
 
     CustomerService customerService
 
+    static allowedMethods = [save: 'POST']
+
     def index() {
         return [customers: customerService.findAll()]
     }
 
     def create() {
         return [:]
+    }
+
+    def save() {
+        try {
+            customerService.save(params)
+            redirect action: 'index'
+        } catch (Exception exception) {
+            exception.printStackTrace()
+            redirect action: 'create'
+        }
     }
 }
