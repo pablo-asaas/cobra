99ee3bfa6608fd45a3423fb3e825acb8a0aff24d
HugoSRCruz <hugo.cruz@asaas.com.br>
2023-05-19 13:54:17 -0300

    Alterando PayerService

--- grails-app/services/payer/PayerService.groovy
+++ grails-app/services/payer/PayerService.groovy
@@ -2,8 +2,6 @@ package payer
 
 import grails.gorm.transactions.Transactional
 
-import java.time.LocalDate
-
 @Transactional
 class PayerService {
 
@@ -15,18 +13,16 @@ class PayerService {
         return Payer.get(id)
     }
 
-    public void save(params){
-
-        if (!params.name) throw new RuntimeException("Nome é obrigatório")
-        if (!params.email) throw new RuntimeException("Email é obrigatório")
-        if (!params.cpfCnpj) throw new RuntimeException("CPF/CNPJ é obrigatório")
-        if (!params.phoneNumber) throw new RuntimeException("Celular é obrigatório")
+    public void save(Map params){
 
         Payer payer = new Payer()
-        payer.name = params.name
-        payer.email = params.email
-        payer.cpfCnpj = params.cpfCnpj
-        payer.phoneNumber = params.phoneNumber
-        payer.save()
+        println params
+
+        if (params.name) payer.name = params.name
+        if (params.email) payer.email = params.email
+        if (params.cpfCnpj) payer.cpfCnpj = params.cpfCnpj
+        if (params.phoneNumber) payer.phoneNumber = params.phoneNumber
+
+        payer.save(failOnError: true)
     }
 }
