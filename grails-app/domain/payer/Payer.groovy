26f6c24c07d14b785c214e0395d806cac7b413d8
HugoSRCruz <hugo.cruz@asaas.com.br>
2023-05-22 14:43:30 -0300

    Adicionando namedQueries em Payer

--- grails-app/domain/payer/Payer.groovy
+++ grails-app/domain/payer/Payer.groovy
@@ -9,4 +9,15 @@ class Payer extends BasePerson{
     static constraints = {
         phoneNumber blank: false, size: 13..13
     }
+
+    static namedQueries = {
+        query { Map search ->
+            if (!Boolean.valueOf(search.includeDeleted)) {
+                eq("deleted", false)
+            }
+            if (search.containsKey("id")) {
+                eq("id", search.id)
+            }
+        }
+    }
 }
