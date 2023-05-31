36e5b2dd92ef862c77145b652b0387e90a9c4be4
HugoSRCruz <hugo.cruz@asaas.com.br>
2023-05-31 09:38:23 -0300

    Tratando ResourceNotFoundException no controller

--- grails-app/controllers/cobra/payer/PayerController.groovy
+++ grails-app/controllers/cobra/payer/PayerController.groovy
@@ -1,6 +1,7 @@
 package cobra.payer
 
 import cobra.exception.BusinessException
+import cobra.exception.ResourceNotFoundException
 import grails.converters.JSON
 import io.micronaut.http.HttpStatus
 
@@ -35,6 +36,8 @@ class PayerController {
         try {
             payerService.delete(id)
             redirect action: "index"
+        }catch(ResourceNotFoundException e){
+            render view: "/notFound", model: [message: e.getMessage()], status: HttpStatus.NOT_FOUND.code
         }catch (Exception e) {
             e.printStackTrace()
             redirect action: "index"
@@ -42,13 +45,22 @@ class PayerController {
     }
 
     def show (Long id) {
-        return [payer: payerService.findById(id)]
+        try {
+            return [payer: payerService.findById(id)]
+        }catch (ResourceNotFoundException e){
+            render view: "/notFound", model: [message: e.getMessage()], status: HttpStatus.NOT_FOUND.code
+        }catch (Exception e) {
+            e.printStackTrace()
+            redirect action: "index"
+        }
     }
 
     def update() {
         try {
             payerService.update(params.id as Long, params)
             render([message: "Editado com sucesso"] as JSON, status: HttpStatus.CREATED.code)
+        }catch (ResourceNotFoundException e){
+            render view: "/notFound", model: [message: e.getMessage()], status: HttpStatus.NOT_FOUND.code
         }catch (BusinessException e) {
             e.printStackTrace()
             render([message: e.message] as JSON, status: HttpStatus.BAD_REQUEST.code)
