2d03324d19d5840bddcafdfdbe35a152ba7be3e4
HugoSRCruz <hugo.cruz@asaas.com.br>
2023-06-06 14:19:11 -0300

    Adicionando metodo verifyRoleUser

--- grails-app/init/cobra/BootStrap.groovy
+++ grails-app/init/cobra/BootStrap.groovy
@@ -1,8 +1,20 @@
 package cobra
 
+import cobra.user.Role
+import grails.gorm.transactions.Transactional
+
 class BootStrap {
 
-    def init = { servletContext ->
+    def init = {
+        verifyRoleUser()
+    }
+    @Transactional
+    void verifyRoleUser() {
+        Role role = Role.find{authority == "ROLE_USER"}
+
+        if(!role) {
+            new Role(authority: "ROLE_USER").save(failOnError: true)
+        }
     }
     def destroy = {
     }
