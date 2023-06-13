f0134873984d1c688ee9e2b64a2cf6609b86fd3d
Pablo <pablo.bayerl@asaas.com.br>
2023-06-13 09:57:43 -0300

    Adicionado BaseController

--- grails-app/controllers/cobra/user/UserController.groovy
+++ grails-app/controllers/cobra/user/UserController.groovy
@@ -1,37 +1,21 @@
 package cobra.user
 
-import cobra.customer.Customer
-import cobra.exception.BusinessException
+import cobra.base.BaseController
 import grails.converters.JSON
-import grails.plugin.springsecurity.SpringSecurityService
 import grails.plugin.springsecurity.annotation.Secured
 import io.micronaut.http.HttpStatus
 
 @Secured('ROLE_USER')
-class UserController {
+class UserController extends BaseController {
 
     UserService userService
-    SpringSecurityService springSecurityService
 
     def index() {
         return [userList: userService.findAll(getCurrentCustomer()), currentUser: springSecurityService.getCurrentUser().username]
     }
 
     def save() {
-        try {
-            userService.save(getCurrentCustomer(), params)
-            render([message: "Usuário criado com sucesso"] as JSON, status: HttpStatus.CREATED.code)
-        }catch (BusinessException e) {
-            e.printStackTrace()
-            render([message: e.message] as JSON, status: HttpStatus.BAD_REQUEST.code)
-        }catch (Exception e){
-            e.printStackTrace()
-            render([message: "Ocorreu um erro desconhecido"] as JSON, status: HttpStatus.BAD_REQUEST.code)
-        }
-    }
-
-    private Customer getCurrentCustomer() {
-        User user = springSecurityService.currentUser
-        return user.customer
+        userService.save(getCurrentCustomer(), params)
+        render([message: "Usuário criado com sucesso"] as JSON, status: HttpStatus.CREATED.code)
     }
 }
