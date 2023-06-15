579c64978527a6965f12dbf33c14748c658e6528
HugoSRCruz <hugo.cruz@asaas.com.br>
2023-06-15 13:25:27 -0300

    Alterando assinatura de metodo save para receber adapter

--- grails-app/controllers/cobra/payer/PayerController.groovy
+++ grails-app/controllers/cobra/payer/PayerController.groovy
@@ -1,6 +1,7 @@
 package cobra.payer
 
 import cobra.base.BaseController
+import cobra.payer.adapter.PayerAdapter
 import grails.converters.JSON
 import grails.plugin.springsecurity.annotation.Secured
 import io.micronaut.http.HttpStatus
@@ -21,7 +22,7 @@ class PayerController extends BaseController {
     }
 
     def save() {
-        payerService.save(getCurrentCustomer(), params)
+        payerService.save(getCurrentCustomer(), new PayerAdapter(params))
         render([message: "Pagador criado com sucesso"] as JSON, status: HttpStatus.CREATED.code)
     }
 
