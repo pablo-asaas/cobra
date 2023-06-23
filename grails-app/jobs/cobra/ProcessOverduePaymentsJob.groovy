8ec612036da2651073cab817c118cb5def649b43
Pablo <pablo.bayerl@asaas.com.br>
2023-06-23 15:55:00 -0300

    Alterado Job de pagamentos para não ser concorrente

--- grails-app/jobs/cobra/ProcessOverduePaymentsJob.groovy
+++ grails-app/jobs/cobra/ProcessOverduePaymentsJob.groovy
@@ -6,6 +6,8 @@ class ProcessOverduePaymentsJob {
 
     PaymentService paymentService
 
+    static concurrent = false
+
     static triggers = {
         cron name: "ProcessOverduePaymentsJobTrigger", startDelay: 10000, cronExpression: "0 0 0 * * ?"
     }
