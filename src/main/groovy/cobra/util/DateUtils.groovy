6375355af8802bc6285cc45a6a377cb9c5ecb772
HugoSRCruz <hugo.cruz@asaas.com.br>
2023-06-23 11:49:38 -0300

    Refatorando DateUtils

--- src/main/groovy/cobra/util/DateUtils.groovy
+++ src/main/groovy/cobra/util/DateUtils.groovy
@@ -43,12 +43,42 @@ class DateUtils {
     }
 
     public static Date getStartOfMonth() {
+        return getStartOfMonth(new Date())
+    }
+
+    public static Date getStartOfMonth(Date date) {
         Calendar calendar = Calendar.getInstance()
+        calendar.setTime(date)
 
         calendar.set(Calendar.DAY_OF_MONTH, 1)
         calendar.set(Calendar.HOUR_OF_DAY, 0)
         calendar.set(Calendar.MINUTE, 0)
         calendar.set(Calendar.SECOND, 0)
+
+        return calendar.getTime()
+    }
+
+    public static Date getEndOfMonth() {
+        return getEndOfMonth(new Date())
+    }
+
+    public static Date getEndOfMonth(Date date) {
+        Calendar calendar = Calendar.getInstance()
+        calendar.setTime(date)
+
+        int monthLastDay = Calendar.getInstance().getActualMaximum(Calendar.DAY_OF_MONTH)
+
+        calendar.set(Calendar.DAY_OF_MONTH, monthLastDay)
+        calendar.set(Calendar.HOUR_OF_DAY, 23)
+        calendar.set(Calendar.MINUTE, 59)
+        calendar.set(Calendar.SECOND, 59)
+
+        return calendar.getTime()
+    }
+
+    public static Date subtractOrAddMonths(int quantity) {
+        Calendar calendar = Calendar.getInstance()
+        calendar.add(Calendar.MONTH, quantity)
 
         return calendar.getTime()
     }
