6ce497d7d90b4215a62d8b2e3f7fbcd3f699f8d5
Pablo <pablo.bayerl@asaas.com.br>
2023-06-26 15:00:25 -0300

    Adicionados filtros de paginação offset e limit aos métodos findAll e findAllDeleted no PaymentService

--- grails-app/services/cobra/payment/PaymentService.groovy
+++ grails-app/services/cobra/payment/PaymentService.groovy
@@ -19,13 +19,13 @@ class PaymentService {
     PaymentNotificationService paymentNotificationService
 
     @ReadOnly
-    public List<Payment> findAll(Customer customer) {
-        return Payment.query([customer: customer]).list()
+    public List<Payment> findAll(Customer customer, Integer offset, Integer limit) {
+        return Payment.query([customer: customer, offset: offset, limit: limit]).list()
     }
 
     @ReadOnly
-    public List<Payment> findAllDeleted(Customer customer) {
-        return Payment.query([customer: customer, onlyDeleted: true]).list()
+    public List<Payment> findAllDeleted(Customer customer, Integer offset, Integer limit) {
+        return Payment.query([customer: customer, onlyDeleted: true, offset: offset, limit: limit]).list()
     }
 
     public Payment findById(Customer customer, Long id) {
